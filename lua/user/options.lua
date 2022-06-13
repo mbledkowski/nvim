a7ca01ae2aa90e7d467726153573feb7283ad5a8
Maciej Błędkowski <pub@mble.dk>
2022-06-13 14:25:13 +0200

    Update options.lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -31,13 +31,13 @@ local options = {
   laststatus = 3,
   showcmd = false,
   ruler = false,
-  -- relativenumber = true,                  -- set relative numbered lines
+  relativenumber = true,                  -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  guifont = "JetBrains\\ Mono:h8",               -- the font used in graphical neovim applications
 
 }
 vim.g.loaded_netrw = 1
